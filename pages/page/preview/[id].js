20419db207f8be6b7928c0ee726d522c3a235461
gsdavris <g.s.davris@gmail.com>
2021-08-13 16:11:00 +0300

    fixed capitalization 2

--- pages/page/preview/[id].js
+++ pages/page/preview/[id].js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 import client from '../../../src/apollo/client';
 import { GET_PAGE_BY_ID} from '../../../src/queries/pages/get-page';
 import Layout from '../../../src/components/layouts/Layout';
