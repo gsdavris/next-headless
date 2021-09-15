ec4dabcc5d28cda89b6df51840c3f835dccba0aa
gsdavris <g.s.davris@gmail.com>
2021-09-15 18:24:01 +0300

    applied google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
+import { useEffect } from 'react';
 import '../src/styles/style.scss';
 import { ApolloProvider } from '@apollo/client';
 import client from '../src/apollo/client';
+import * as ga from '../src/utils/ga';
 
-import Router from 'next/router';
+import Router, { useRouter } from 'next/router';
 import NProgress from 'nprogress';
 import { motion } from 'framer-motion';
 
@@ -12,6 +14,23 @@ Router.events.on( 'routeChangeComplete', () => NProgress.done() );
 Router.events.on( 'routeChangeError', () => NProgress.done() );
 
 function MyApp( { Component, pageProps, router } ) {
+	const myRouter = useRouter();
+
+	useEffect( () => {
+		const handleRouteChange = ( url ) => {
+			ga.pageview( url );
+		};
+		//When the component is mounted, subscribe to router changes
+		//and log those page views
+		myRouter.events.on( 'routeChangeComplete', handleRouteChange );
+
+		// If the component is unmounted, unsubscribe
+		// from the event with the `off` method
+		return () => {
+			myRouter.events.off( 'routeChangeComplete', handleRouteChange );
+		};
+	}, [ myRouter.events ] );
+
 	return (
 		<ApolloProvider client={client}>
 			<motion.div key={router.asPath} initial="pageInitial" animate="pageAnimate" variants={{
